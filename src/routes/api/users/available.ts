02f2c30d8d4fa23385a480866452237f49166b28
Nixken463 <161881576+Nixken463@users.noreply.github.com>
2026-04-23 20:41:28 +0000

    rename index.ts to available.ts under users

---
+++ src/routes/api/users/available.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import type { Env } from "src/types/hono";
+import Responses from "src/utils/responses";
+const searchUsers = new Hono<Env>
+
+searchUsers.get("/", async (c) =>{
+    const db = c.get('db')
+    const r = new Responses(c)
+    const username = c.req.query('username')
+    const userId = c.get('userId')
+
+    const result = await db.query`SELECT username,userId FROM users WHERE username LIKE ${username + "%"} AND userId != ${userId} LIMIT 10`
+    return r.data(result,200)
+})
+export default searchUsers
