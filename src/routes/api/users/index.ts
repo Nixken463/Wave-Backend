9fe41a088a6795e1bf8ff7ee27f729cb2b778335
Nixken463 <161881576+Nixken463@users.noreply.github.com>
2026-04-23 15:58:48 +0000

    fix finding own account on user search

--- src/routes/api/users/index.ts
+++ src/routes/api/users/index.ts
@@ -7,10 +7,11 @@ searchUsers.get("/", async (c) =>{
     const db = c.get('db')
     const r = new Responses(c)
     const username = c.req.query('username')
+    const userId = c.get('userId')
     if (!username || username.length === 0){
         return r.error("MissingParams", 400)
     }
-    const result = await db.query`SELECT username,userId FROM users WHERE username LIKE ${username + "%"} LIMIT 10`
+    const result = await db.query`SELECT username,userId FROM users WHERE username LIKE ${username + "%"} AND userId != ${userId} LIMIT 10`
     return r.data(result,200)
 })
 export default searchUsers
